d1a64b4c0945de67bb730f7e75fc094cccb92b8e
Paul Franco <paulfranco@me.com>
2018-07-26 00:54:34 -0700

    Updated UsersReceived Class

--- app/src/main/java/co/paulfran/paulfranco/stackoverflowtopfive/model/UsersReceived.java
+++ app/src/main/java/co/paulfran/paulfranco/stackoverflowtopfive/model/UsersReceived.java
@@ -1,4 +1,20 @@
 package co.paulfran.paulfranco.stackoverflowtopfive.model;
 
+import java.util.List;
+
 public class UsersReceived {
+
+    // List Of Users Received from API
+    @SerializedName("items")
+    private List<User> users;
+
+    // Getters and setters
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
 }
