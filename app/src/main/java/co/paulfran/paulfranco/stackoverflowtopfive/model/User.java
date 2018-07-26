fc5b63d677cb8e6b3293c59406bb86885798c841
Paul Franco <paulfranco@me.com>
2018-07-26 00:52:30 -0700

    Updated User Class

--- app/src/main/java/co/paulfran/paulfranco/stackoverflowtopfive/model/User.java
+++ app/src/main/java/co/paulfran/paulfranco/stackoverflowtopfive/model/User.java
@@ -1,4 +1,52 @@
 package co.paulfran.paulfranco.stackoverflowtopfive.model;
 
+import java.util.HashMap;
+
 public class User {
+
+    @SerializedName("location")
+    private String location;
+
+    @SerializedName("display_name")
+    private String userName;
+
+    @SerializedName("reputation")
+    private String reputation;
+
+    @SerializedName("badge_counts")
+    private HashMap<String,Integer> badges = new HashMap<>();
+
+
+    // GETTERS AND SETTERS
+    public HashMap<String, Integer> getBadges() {
+        return badges;
+    }
+
+    public void setBadges(HashMap<String, Integer> badges) {
+        this.badges = badges;
+    }
+
+    public String getReputation() {
+        return reputation;
+    }
+
+    public void setReputation(String reputation) {
+        this.reputation = reputation;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
