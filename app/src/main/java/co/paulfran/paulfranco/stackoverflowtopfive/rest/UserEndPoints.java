047bfc66ba12202dea4545411ead3615089a2cef
Paul Franco <paulfranco@me.com>
2018-07-26 01:07:25 -0700

    updated activity_main.xml and created list_item_user.xml

--- app/src/main/java/co/paulfran/paulfranco/stackoverflowtopfive/rest/UserEndPoints.java
+++ app/src/main/java/co/paulfran/paulfranco/stackoverflowtopfive/rest/UserEndPoints.java
@@ -1,4 +1,11 @@
 package co.paulfran.paulfranco.stackoverflowtopfive.rest;
 
+import co.paulfran.paulfranco.stackoverflowtopfive.model.UsersReceived;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface UserEndPoints {
+    @GET("/2.2/users?page=1&pagesize=5&order=desc&site=stackoverflow")
+    Call<UsersReceived> getUsers(@Query("sort") String sort);
 }
