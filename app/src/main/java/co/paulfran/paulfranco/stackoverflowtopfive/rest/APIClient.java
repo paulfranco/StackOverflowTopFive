230b19e919802ad175d8f8e50f047aa485f2c08b
Paul Franco <paulfranco@me.com>
2018-07-26 00:56:01 -0700

    Updated APIClient Class

--- app/src/main/java/co/paulfran/paulfranco/stackoverflowtopfive/rest/APIClient.java
+++ app/src/main/java/co/paulfran/paulfranco/stackoverflowtopfive/rest/APIClient.java
@@ -1,4 +1,17 @@
 package co.paulfran.paulfranco.stackoverflowtopfive.rest;
 
 public class APIClient {
+    public static final String BASE_URL = "https://api.stackexchange.com";
+    private static Retrofit retrofit = null;
+
+
+    public static Retrofit getClient() {
+        if (retrofit==null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
 }
